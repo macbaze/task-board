bf72bcea834603d593aae1b8362b79e8278f75ee
macbaze <dav.taras@gmail.com>
2019-10-14 20:21:28 +0300

    added edit page

---
+++ edit.php
@@ -0,0 +1,17 @@
+<?php
+include('backend/auth.php');
+if ($adminAccount && isset($_GET['id'])) {
+  $options = array(
+    'options' => array(
+        'min_range' => 1
+    )
+  );
+  if ($id = filter_var($_GET['id'], FILTER_VALIDATE_INT, $options)) {
+    include('backend/task.php');
+    $task = getOneTask($id);
+    include('view/editTask.php');
+    exit; //prevent redirect
+  }
+}
+header("Location: current.php"); //will redirect only if there are problems
+?>
---
+++ view/editTask.php
@@ -0,0 +1,32 @@
+<?php
+$pageTitle = 'Редактирование задачи';
+include('parts/pageHeader.php');
+include('parts/menu.php');
+?>
+<form class="needs-validation mx-auto" method="POST" name="editTask" novalidate>
+  <div class="form-group">
+    <label for="uname">Имя пользователя</label>
+    <input type="text" class="form-control" id="uname" placeholder="Введите имя пользователя" name="username" required value="<?=$task->user?>">
+    <div class="invalid-feedback">Поле должно быть заполнено</div>
+  </div>
+  <div class="form-group">
+    <label for="email">E-Mail</label>
+    <input type="email" class="form-control" id="email" placeholder="Введите E-Mail" name="email" required value="<?=$task->email?>">
+    <div class="invalid-feedback">Должен быть указан правильный E-Mail</div>
+  </div>
+  <div class="form-group">
+    <label for="textarea">Текст задачи</label>
+    <textarea class="form-control" id="textarea" rows="3" placeholder="Введите текст задачи" name="text" required><?=$task->text?></textarea>
+    <div class="invalid-feedback">Поле должно быть заполнено</div>
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="done" name="done"<?=($task->done == 1)?' checked':'';?>>
+    <label class="form-check-label" for="done">Отметка о выполнении</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Сохранить</button>
+</form>
+<script src="js/formValidation.js"></script>
+<script src="js/editTaskFormPOST.js"></script>
+<?php
+include('parts/pageFooter.php');
+?>
